09fbeb473aa985f1c920dd6fb7200b197d32ca14
jvalcher <jvalcher@gmail.com>
2025-12-25 10:50:49 -0600

    CATEG_ -> GHOST_

--- inc/src/save_dirs.h
+++ inc/src/save_dirs.h
@@ -15,18 +15,19 @@ int create_save_dirs(void);
 /*
     Get save directory path
     ---------
-    - categ: category string (path.h), ignored for CATEG_DEF type so can be NULL
-    - type: CATEG_DEF, CATEG_REC, CATEG_USR
+    - track_categ: track, category string (track_categ.h)
+        - Ignored for GHOST_DEF type so can be NULL
+    - ghost_type: GHOST_DEF, GHOST_REC, GHOST_USR
     - Returns path string, NULL on error
 */
-#define CATEG_DEF  0  // Default, non-ghost play
-#define CATEG_REC  1  // Record-holding ghosts
-#define CATEG_USR  2  // User ghosts
+#define GHOST_DEF  0  // Default, non-ghost play
+#define GHOST_REC  1  // Record-holding ghosts
+#define GHOST_USR  2  // User ghosts
     //
-char* get_save_dir_path(const char *categ, int type);
+char* get_save_dir_path(const char *track_categ, int ghost_type);
 
 /*
-    Free resources
+    Free resources (not directories)
 */
 void save_dirs_clean(void);
 
